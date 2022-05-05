8fbfe6406f5a6739e0660ee879a1bba247fa4cd8
Ludi-1 <liou.hu@protonmail.com>
2022-05-06 01:58:07 +0200

    learning-gem5: part2 done+fix add base/trace.hh, else DPRINTF will not work
    Part2, Debugging gem5 done

--- src/learning_gem5/part2/hello_object.cc
+++ src/learning_gem5/part2/hello_object.cc
@@ -1,13 +1,13 @@
 #include "learning_gem5/part2/hello_object.hh"
-
-#include <iostream>
+#include "debug/HelloExample.hh"
+#include "base/trace.hh"
 
 namespace gem5
 {
 HelloObject::HelloObject(const HelloObjectParams &params) :
     SimObject(params)
 {
-    std::cout << "Hello World! From a SimObject!" << std::endl;
+    DPRINTF(HelloExample, "Created the hello object\n");
 }
 
 } // namespace gem5
